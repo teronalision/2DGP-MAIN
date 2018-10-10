002f7248d5907ae429e9650e4b01d4be46a7900e
teronalision <teronalision@gmail.com>
2018-10-10 16:11:56 +0900

    시작

---
+++ main.py
@@ -0,0 +1,25 @@
+from pico2d import *
+open_canvas()
+
+
+#초기화
+
+
+
+
+
+#게임 본체
+while True:
+    get_events()
+
+
+    
+    clear_canvas()
+    update_canvas
+    delay(0.05)
+    
+
+
+
+#종료
+close_canvas()
