eae9baae3c79292478b825a9956bf7d671973835
teronalision <teronalision@gmail.com>
2018-10-18 02:02:12 +0900

    총알 구현
    이동 함수, 생성 함수, 명령 함수

--- BULLET.py
+++ BULLET.py
@@ -3,8 +3,15 @@
 class bullet:
 
     
-    def __init__(self):
-        self.x, self.y = 400, 500
+    def __init__(self,x,y,i):
+        self.x, self.y = x, y
         self.vx, self.vy = 0, 0
-        self.image = 0
+        self.image = i
 
+    def order(self,x,y):
+        self.vx, self.vy = x, y
+
+    def update(self):
+
+        self.x += self.vx
+        self.y += self.vy
--- 기본.py
+++ 기본.py
@@ -4,15 +4,15 @@ import HERO
 import BULLET
 
 image = None
-b = [BULLET.bullet()]
+b = [BULLET.bullet(400,500,1), BULLET.bullet(300,500,1), BULLET.bullet(500,500,1)]
 bimage = None
+time = 0
 
 def start():
     global image, bimage
     image = load_image('test.png')
-    HERO.vy = 1
-    HERO.x = 400
-    bimage = [load_image('dumy_b.png')]
+    HERO.x,HERO.y = 400, 300
+    bimage = [load_image('dumy_b.png'), load_image('dumy_c.png')]
 
 
 
@@ -33,12 +33,28 @@ def handle():
 
 
 def update():
+    global time
+
+    #캐릭터 이동
     HERO.update()
 
     for obj in b:
+        #탄 이동
+        obj.update()
+
+        #충돌판정
         if (HERO.x - obj.x)**2 + (HERO.y - obj.y)**2 < 60**2:
             HERO.live = False
     
+
+    if time>10:
+        b[0].order(0,-1)
+    if time>20:
+        b[1].order(1,-1)
+    if time>30:
+        b[2].order(-1,-1)
+
+    time +=1
 
 
 def draw():
