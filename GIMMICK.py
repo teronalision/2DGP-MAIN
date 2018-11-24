6add3af8b37e355ad062b09c3002117f77b1854f
teronalision <teronalision@gmail.com>
2018-11-24 22:01:20 +0900

    보스 스테이지 별로 분리
    객체와 이동으로 분리

--- GIMMICK.py
+++ GIMMICK.py
@@ -2,14 +2,16 @@ import ENGINE
 import ZAKO
 
 time = 0.0
+stage_num = 0
 sponer = ZAKO.Monster_sponer()
 
 doing = []
 
-boss = []
+move_boss = []
+boss = None
 
 def init():
-    global time, doing, boss
+    global time, doing, move_boss
     time= 0
     #시간, 실행내용
     doing = [(4+(i*0.3),(ZAKO.STIRGE,0,600,ZAKO.RD)) for i in range(10)]+[
@@ -17,31 +19,38 @@ def init():
     for i in range(10):
         doing.append((15+(i*0.3),(ZAKO.STIRGE,0,550,ZAKO.R)))
         doing.append((15+(i*0.3),(ZAKO.STIRGE,500,500,ZAKO.L)))
-    doing += [(20,(ZAKO.FAIRY,100,600,ZAKO.D_400)),(20,(ZAKO.FAIRY,400,600,ZAKO.D_400)),
-              (30,(ZAKO.WRAITH,250,600,ZAKO.D_400))]
+    doing += [(20,(ZAKO.FAIRY,100,600,ZAKO.D_400)),(20,(ZAKO.FAIRY,400,600,ZAKO.D_400))]
 
 
     #보스
-    boss = [(33,ZAKO.HIFEL),
-            (35,ZAKO.HIFER)]
+    b1 = ZAKO.WRAITH
+    b2
+    b3
+    boss = [b1,b2,b3]
+
+    move_boss = [(63,ZAKO.HIFEL),
+                 (70,ZAKO.HIFER)]
 
 
 def run_stage():
-    global time, doing
-    if len(doing) == 0:
-        if(time > boss[0][0]):
-            ENGINE.object_list[1][0].moving = boss[0][1]
-            boss.pop
+    global time, doing, move_boss
+
+    if time <60:
+        if time > doing[0][0]:
+            a,b,c,d = doing[0][1]
+            sponer.add_monster(a,b,c,d)
+            doing.remove(doing[0])
+
+    elif(time > boss[0][0]):
+        boss[stage_num].moving = move_boss[0][1]
+        move_boss.remove(move_boss[0])
 
 
 
         if(len(ENGINE.object_list[1]) == 0):
             return True
 
-    elif time > doing[0][0]:
-        a,b,c,d = doing[0][1]
-        sponer.add_monster(a,b,c,d)
-        doing.remove(doing[0])
+    
 
 
 
