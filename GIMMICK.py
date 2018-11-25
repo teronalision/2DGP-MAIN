4facd1b47fd35747ba33695533bca9621428ee96
teronalision <teronalision@gmail.com>
2018-11-25 17:42:05 +0900

    리스트 끝 부분 명시

--- GIMMICK.py
+++ GIMMICK.py
@@ -13,7 +13,7 @@ snext = False
 
 def init():
     global time, field_stage, boss_stage,boss, boss_idx, doing
-    time= 30
+    time= 0
     boss_idx = 0
     #시간, 실행내용
     field_stage = [(4+(i*0.3),(ZAKO.STIRGE,0,600,ZAKO.RD)) for i in range(10)]+[
@@ -21,14 +21,16 @@ def init():
     for i in range(10):
         field_stage.append((15+(i*0.3),(ZAKO.STIRGE,0,550,ZAKO.R)))
         field_stage.append((15+(i*0.3),(ZAKO.STIRGE,500,500,ZAKO.L)))
-    field_stage += [(20,(ZAKO.FAIRY,100,600,ZAKO.D_400)),(20,(ZAKO.FAIRY,400,600,ZAKO.D_400))]
+    field_stage += [(20,(ZAKO.FAIRY,100,600,ZAKO.D_400)),(20,(ZAKO.FAIRY,400,600,ZAKO.D_400)),
+                    (60,None)]
 
 
     #보스
     if(ENGINE.stage_num == 0):
         boss = sponer.add_monster(ZAKO.WRAITH, 250,600,None,False)
-        boss_stage = [(63,ZAKO.HIFEL),
-                      (70,ZAKO.HIFER)]
+        boss_stage = [(0,ZAKO.HIFEL),
+                      (10,ZAKO.HIFER),
+                      (20,None)]
 
 
     #
@@ -64,14 +66,15 @@ def run_stage():
 
 def play_order(tuple):
     global boss
-    if(len(tuple) == 4):
-         a,b,c,d = tuple
-         sponer.add_monster(a,b,c,d)
+    if snext == False:
+        if tuple == None:
+            return
+        a,b,c,d = tuple
+        sponer.add_monster(a,b,c,d)
 
     else:
-         boss.moving = tuple[1]
+        boss.moving = tuple
 
-    pass
 
 def summon_boss():
     if len(ENGINE.object_list[1])>0:
