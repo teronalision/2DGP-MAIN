d4c7de95ce45cab446d2c1ad83cc8301c00f911e
teronalision <teronalision@gmail.com>
2018-11-25 17:25:04 +0900

    코드 압축 , 정리

--- GIMMICK.py
+++ GIMMICK.py
@@ -9,7 +9,7 @@ doing = None
 field_stage = []
 boss_stage = []
 boss = None
-boss_idx = 0
+snext = False
 
 def init():
     global time, field_stage, boss_stage,boss, boss_idx, doing
@@ -36,46 +36,45 @@ def init():
 
 
 def run_stage():
-    global time, field_stage, boss_stage, boss,boss_idx
+    global doing, time, field_stage, boss_stage, boss, snext
 
 
-    if len(field_stage) != 0:
-        if time > field_stage[0][0]:
-            a,b,c,d = field_stage[0][1]
-            sponer.add_monster(a,b,c,d)
-            field_stage.remove(field_stage[0])
-    elif(int(time) == 60):
-        if len(ENGINE.object_list[1])>0:
-            ENGINE.object_list[1].clear
-
-        ENGINE.add_obj(boss,1)
-        time = 61
-
-    elif(boss.hp >0):
-        if(time > boss_stage[boss_idx][0]):
-            boss.moving = boss_stage[boss_idx][1]
-        
-        #끝나면 다시 처음부터
-        if(len(boss_stage) <= boss_idx):
-            boss_idx = 0
-            time = 61
-
-
-
-    
-    else:
+    if(boss.hp <= 0):
         return True
+
+
+
+    if len(doing) != 0:
+        if time > doing[0][0]:
+            play_order(doing[0][1])
+            doing.remove(doing[0])
+
+    elif(snext == False):
+        summon_boss()
+        snext = True
+
+    else:
+        doing += boss_stage
+        time = 0  
+    
 
     time += ENGINE.frame_time
     return False
 
 
 def play_order(tuple):
+    global boss
     if(len(tuple) == 4):
          a,b,c,d = tuple
          sponer.add_monster(a,b,c,d)
 
     else:
-         boss.moving = boss_stage[boss_idx][1]
+         boss.moving = tuple[1]
 
     pass
+
+def summon_boss():
+    if len(ENGINE.object_list[1])>0:
+        ENGINE.object_list[1].clear
+
+    ENGINE.add_obj(boss,1)
